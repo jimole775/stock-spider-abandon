caac28a40c29b599d67702499fb3e61a15408268
Andy-Super <727346595@163.com>
2019-08-16 00:14:22 +0800

    增加几个需求

--- test2/merge.js
+++ test2/merge.js
@@ -1,5 +1,11 @@
-const baseHq = require('../db/base_hq.json')
-const baseDisHq = require('../db/base_dishq.json')
+const baseHq = require('../src/db/base_hq.json')
+const baseDisHq = require('../src/db/base_dishq.json')
 const temp = require('../temp.json')
 const fs = require('fs')
 
+temp.forEach(element => {
+    baseHq[element.code] = Object.assign(baseHq[element.code], element)
+    console.log(baseHq[element.code])
+});
+
+fs.writeFileSync('./src/db/base_hq.json', JSON.stringify(baseHq))
