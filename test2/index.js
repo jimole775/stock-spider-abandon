7fcfd79e76b18f86d883d879f0c89084d98e84af
jimole775 <727346595@qq.com>
2019-08-21 19:09:49 +0800

    刷新几个数据

--- test2/index.js
+++ test2/index.js
@@ -40,12 +40,24 @@ function spillSingleModel(data, code, name) {
         amplitudeAvg: 0, // 平均振幅
         riseRateAvg: 0, // 平均上涨的幅度
         fallRateAvg: 0, // 平均下跌的幅度
-        isMined: 0, // 0代表未暴雷股，1代表暴雷股
-        seriesLimitFallTimes: 0,
+        minedTimes: 0, // 暴雷次数
+        seriesLimitFallTimes: 0, // 连续跌停多少天
+        seriesLimitFallStartPrice: 0, // 连续跌停开始的价格
+        seriesLimitFallStartDate: null, // 连续跌停开始的日期
+        seriesLimitFallRollbackDate: null, // 连续跌停之后，经过多少天之后才恢复元气
+        // seriesFallStopDate_afterLimitFall: null, //@todo 暴雷后的止跌日期
+        // seriesFallStopPrice_afterLimitFall: 0, //@todo 暴雷后的止跌位
+        seriesLimitRiseTimes: 0, // 连续涨停多少天
+        seriesLimitRiseStartPrice: 0, // 连续涨停开始的价格
+        seriesLimitRiseStartDate: null, // 连续涨停开始的日期
+        seriesLimitRiseRollbackDate: null, // 连续跌停之后，经过多少天之后才回落到原来的位置
+        // seriesMiniFallTimes: 0, //@todo 记录最长阴跌次数，如果中间出现超过3次连阳，或者股价超过起跌价，或者有涨停，就不记录（）
+        seriesBoradTimes: 0,
         temp_riseRateSum: 0,
         temp_fallRateSum: 0,
         temp_amplitudeSum: 0,
         temp_seriesLimitFallTimes: 0,
+        temp_seriesLimitRiseTimes: 0,
     }  
     data.forEach((item) => {
         const rowSplit = item.split(',')
@@ -84,25 +96,54 @@ function spillSingleModel(data, code, name) {
 
         // 判断是否有连续3天跌幅超过9%
         if (priceDivdRate < -0.09) {
-
             model.temp_seriesLimitFallTimes ++
+
             model.seriesLimitFallTimes = 
                 model.temp_seriesLimitFallTimes > model.seriesLimitFallTimes ? 
                 model.temp_seriesLimitFallTimes : model.seriesLimitFallTimes
+
             if (model.temp_seriesLimitFallTimes >= 3) {                
                 console.log('跌幅超过9%超3次')
-                model.seriesLimitFallDate = date
-                model.seriesLimitFallPrice = endPrice
-                model.isMined = 1
+                if (!model.seriesLimitFallStartPrice) model.seriesLimitFallStartPrice = startPrice * 1.1 * 1.1                
+                if (!model.seriesLimitFallStartDate) model.seriesLimitFallStartDate = date
+                model.minedTimes ++
             }
         } else {
             model.temp_seriesLimitFallTimes = 0
         }
 
         // 比较暴雷超跌后，股价回滚需要多久
-        if (model.seriesLimitFallDate && !model.seriesLimitFallRollbackDate) {
-            if (endPrice >= model.seriesLimitFallPrice) {
+        if (model.seriesLimitFallStartDate && 
+            !model.seriesLimitFallRollbackDate &&
+            model.temp_seriesLimitFallTimes === 0) {
+            if (endPrice >= model.seriesLimitFallStartPrice) {
                 model.seriesLimitFallRollbackDate = date
+            }
+        }
+
+        // 判断是否有连续3天涨幅超过9%
+        if (priceDivdRate > 0.09) {
+            model.seriesLimitRiseStartPrice = startPrice
+            model.temp_seriesLimitRiseTimes ++
+            model.seriesLimitRiseTimes = 
+                model.temp_seriesLimitRiseTimes > model.seriesLimitRiseTimes ? 
+                model.temp_seriesLimitRiseTimes : model.seriesLimitRiseTimes
+            if (model.temp_seriesLimitRiseTimes >= 3) {                
+                console.log('涨幅超过9%超3次')                
+                if (!model.seriesLimitRiseStartPrice) model.seriesLimitRiseStartPrice = startPrice / 1.1 / 1.1                
+                if (!model.seriesLimitRiseStartDate) model.seriesLimitRiseStartDate = date
+                model.seriesBoradTimes ++
+            }
+        } else {
+            model.temp_seriesLimitRiseTimes = 0
+        }
+        
+        // 比较连板后，股价回滚需要多久
+        if (model.seriesLimitRiseStartDate && 
+            !model.seriesLimitRiseRollbackDate &&
+            model.temp_seriesLimitRiseTimes === 0) {
+            if (endPrice <= model.seriesLimitRiseStartPrice) {
+                model.seriesLimitRiseRollbackDate = date
             }
         }
 
@@ -113,10 +154,20 @@ function spillSingleModel(data, code, name) {
     model.fallRateAvg = (Math.round(- model.temp_fallRateSum / data.length * 10000) / 100).toFixed(2) + '%'
     model.amplitudeAvg = (Math.round(model.temp_amplitudeSum / data.length * 10000) / 100).toFixed(2) + '%'
     model.amplitudeTop = (Math.round(model.amplitudeTop * 10000) / 100).toFixed(2) + '%'
+
+    // // 如果没有暴雷，就置空暴雷的开始价格，没有暴雷，这个值就没有必要存在
+    // if (model.minedTimes === 0) {
+    //   delete model.seriesLimitFallStartPrice
+    // }
+    return clearDust(model)
+}
+
+function clearDust(model) {
     delete model.temp_riseRateSum
     delete model.temp_fallRateSum
     delete model.temp_amplitudeSum
     delete model.temp_seriesLimitFallTimes
+    delete model.temp_seriesLimitRiseTimes
     return model
 }
 
